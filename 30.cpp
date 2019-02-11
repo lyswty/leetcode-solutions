baf18bae78ae0f3ffb96b0ca2c7deadb61c5c665
lyswty <68141859@qq.com>
2019-02-11 17:37:36 +0800

    Create 30.cpp

---
+++ 30.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int>out;
+        if(words.empty()||!s.length()) return out;
+        unordered_map<string,int>cnt;
+        for(int i=0;i<words.size();i++) cnt[words[i]]++;
+        int n=words[0].length(),l=words.size()*n;
+        for(int i=0;i+l<=s.size();i++){
+            unordered_map<string,int>residue=cnt;
+            bool issubstr=true;
+            for(int j=i;j<i+l;j+=n){
+                string curword=s.substr(j,n);
+                if(residue[curword]) residue[curword]--;
+                else{
+                    issubstr=false;
+                    break;
+                }
+            }
+            if(issubstr) out.push_back(i);
+        }
+        return out;
+    }
+};
+static int a=[](){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    return 0;
+}();
