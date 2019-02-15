83899f743a6813c3f0a7849eae95ef837fb0e3a6
lyswty <68141859@qq.com>
2019-02-15 17:24:14 +0800

    Update 44.cpp

--- 44.cpp
+++ 44.cpp
@@ -13,3 +13,25 @@ public:
         return dp[s.size()][p.size()];
     }
 };
+
+//一开始用了用递归回溯，超时
+
+/*
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(!s.size()){
+            for(int i=0;i<p.size();i++) if(p[i]!='*') return false;
+            return true;
+        }
+        if(!p.size()) return false;
+        if(p[0]=='?'||s[0]==p[0]) return isMatch(s.substr(1),p.substr(1));
+        if(p[0]!='*') return false;
+        int j=1;
+        for(;j<p.size()&&p[j]=='*';j++);
+        if(j==p.size()) return true;
+        for(int i=0;i<s.size();i++) if(isMatch(s.substr(i),p.substr(j))) return true;
+        return false;
+    }
+};
+*/
