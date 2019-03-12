a64fb8bb7f47842db8346b0a9479eed4d779696c
lyswty <68141859@qq.com>
2019-03-12 15:20:32 +0800

    Update 116.cpp

--- 116.cpp
+++ 116.cpp
@@ -20,27 +20,11 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        Node* T=root;
-        if(!T) return root;
-        queue<Node*>s;
-        s.push(T);
-        Node *last=root,*l=NULL;
-        while(s.size()){
-            T=s.front();
-            s.pop();
-            if(T->left){
-                s.push(T->left);
-                if(l) l->next=T->left;
-                s.push(T->right);
-                T->left->next=T->right;
-                l=T->right;
-            }
-            else break;
-            if(T==last){
-                last=l;
-                l=NULL;
-            }
-        }
+        if(!root||!root->left) return root;
+        root->left->next=root->right;
+        if(root->next) root->right->next=root->next->left;
+        connect(root->left);
+        connect(root->right);
         return root;
     }
 };
