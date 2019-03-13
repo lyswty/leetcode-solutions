bdcaf57232b0e9ea6c3089e922a7e2d6d60d8582
lyswty <68141859@qq.com>
2019-03-13 13:37:22 +0800

    Create 120.cpp

---
+++ 120.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.empty()) return 0;
+        for(int i=(int)triangle.size()-2;i>=0;i--) for(int j=0;j<triangle[i].size();j++) triangle[i][j]+=triangle[i+1][j]<triangle[i+1][j+1]?triangle[i+1][j]:triangle[i+1][j+1];
+        return triangle[0][0];
+    }
+};
