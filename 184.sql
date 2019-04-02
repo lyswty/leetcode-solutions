799136d488c48223435e683d94fbb5128c6a819d
lyswty <68141859@qq.com>
2019-04-02 12:27:14 +0800

    Create 184.sql

---
+++ 184.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select d.Name as Department, e.Name as Employee, e.Salary from Employee e, Department d where e.DepartmentId=d.Id and (e.Salary, e.DepartmentId) in (select max(Salary), DepartmentId from Employee group by DepartmentId)
