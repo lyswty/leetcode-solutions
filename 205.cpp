2118dfcf11f9b37ab202345bc8bb5092f8dc3f4b
lyswty <68141859@qq.com>
2019-04-09 13:59:44 +0800

    Create 205.cpp

---
+++ 205.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char>mp1,mp2;
+        for(int i=0;i<s.size();i++){
+            if(mp1.find(s[i])==mp1.end()) mp1[s[i]]=t[i];
+            else if(mp1[s[i]]!=t[i]) return false;
+            if(mp2.find(t[i])==mp2.end()) mp2[t[i]]=s[i];
+            else if(mp2[t[i]]!=s[i]) return false;
+        }
+        return true;
+    }
+};
