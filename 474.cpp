6f041cb9811c9ba195b7f446a07842a3e49fd710
lyswty <68141859@qq.com>
2019-06-11 16:23:22 +0800

    Create 474.cpp

---
+++ 474.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    static bool cmp(const string &a,const string &b){
+        return a.size()<b.size();
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(string &s:strs){
+            int zero=0,one=0;
+            for(char x:s){
+                if(x=='0') zero++;
+                else one++;
+            }
+            for(int i=m;i>=zero;i--) for(int j=n;j>=one;j--) dp[i][j]=max(dp[i][j],dp[i-zero][j-one]+1);
+        }
+        return dp[m][n];
+    }
+};
